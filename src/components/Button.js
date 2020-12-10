054ecd72af31af61ae0253bdd41a59e3161b95ce
Lydia <lychicherova@gmail.com>
2020-12-10 09:36:21 -0500

    build DayListItem component, test DayListItem

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,24 +2,24 @@ import React from "react";
 
 import "components/Button.scss";
 
+//import "classnames";
+var classNames = require('classnames');
 
 export default function Button(props) {
-   let buttonClass = "button";
- 
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   }
- 
-   if (props.danger) {
-      buttonClass += " button--danger";
-    }
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   });
+
 
    return (
       <button
          className={buttonClass}
-         onClick={props.onClock}
+         onClick={props.onClick}
          disabled={props.disabled}
       >
       {props.children}
-      </button>);
+      </button>
+      );
+      
  }
