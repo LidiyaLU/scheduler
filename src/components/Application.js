88ee2db6bc108ecb0b7e2fa367e99920d8283878
Lydia <lychicherova@gmail.com>
2020-12-13 20:30:53 -0500

    W07D2: The Schedule - add hardcoded data with appointment and map Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,54 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Ivanov Ivan",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "4pm",
+    interview: {
+      student: "Farida Asfx",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "6pm",
+  }
+];
 
 const days = [
   {
@@ -24,6 +71,15 @@ const days = [
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+
+  const appointmentsAll = appointments.map(appointment => {
+      return (
+        <Appointment
+          key={appointment.id}
+          {...appointment} 
+        />
+      );
+      })
   
   return (
    
@@ -48,7 +104,7 @@ export default function Application(props) {
 </React.Fragment>}
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentsAll}
       </section>
     </main>
   );
